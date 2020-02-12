02238b8bd307f5a0d7bc991cb1e751662a146fcb
jaker <jakekjohnson@outlook.com>
2020-02-11 20:13:54 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+let card1 = "queen";
+let card2 = "queen";
+let card3 = "king";
+let card4 = "king";
+
+console.log("User flipped " + card1);
+console.log("User flipped " + card3);
