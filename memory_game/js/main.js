49dd38e89face11489e20ff207b3076beb4e187a
jaker <jakekjohnson@outlook.com>
2020-02-11 21:42:59 -0800

    added objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
+
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -11,9 +33,9 @@ function checkForMatch() {
 
 function flipCard(cardId) {
 
-  console.log("User flipped " +cards[cardId]);
+  console.log("User flipped " +cards[cardId].rank);
 
-  cardsInPlay.push(cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
 
   if (cardsInPlay.length === 2) {
     checkForMatch();
